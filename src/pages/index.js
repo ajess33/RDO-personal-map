dc0a75e7d3f97032de4c03ed6d145eadcf3bc2c5
Austin Jess <ajess33@gmail.com>
2018-12-23 22:06:50 -0600

    added map component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,36 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Map, ImageOverlay } from 'react-leaflet'
+import L from 'leaflet'
+import RDRMap from '../images/rdr2-map.png'
+import RDRMapFull from '../images/rdr2-map-full.png'
+import '../styles/index.css'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
+// width: 7200 height: 5400
+// credit: https://www.gtabase.com/red-dead-redemption-2/locations/red-dead-redemption-2-map-full-official-world-map-of-rdr2-in-hd
+
+const bounds = [[0, 0], [2000, 2000]]
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <div>
+    <MainMap />
+  </div>
 )
+
+class MainMap extends React.Component {
+  render() {
+    if (typeof window !== 'undefined') {
+      return (
+        <Map id="mapid" crs={L.CRS.Simple} minZoom={-2} bounds={bounds}>
+          <ImageOverlay minZoom={-2} bounds={bounds} url={RDRMapFull} />
+        </Map>
+      )
+    }
+    return (
+      <div>
+        <p>Wrong</p>
+      </div>
+    )
+  }
+}
 
 export default IndexPage
