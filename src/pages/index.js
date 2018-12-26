019bc51a029773da31b84613fb4cb0a2675999ff
Austin Jess <ajess33@gmail.com>
2018-12-26 17:16:43 -0600

    added dynamic markers

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Map, ImageOverlay } from 'react-leaflet'
+import { Map, ImageOverlay, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
 import RDRMap from '../images/rdr2-map.png'
 import RDRMapFull from '../images/rdr2-map-full.png'
@@ -8,7 +8,7 @@ import '../styles/index.css'
 // width: 7200 height: 5400
 // credit: https://www.gtabase.com/red-dead-redemption-2/locations/red-dead-redemption-2-map-full-official-world-map-of-rdr2-in-hd
 
-const bounds = [[0, 0], [2100, 3600]]
+const bounds = [[-400, -600], [2400, 3800]]
 
 const IndexPage = () => (
   <div>
@@ -17,24 +17,40 @@ const IndexPage = () => (
 )
 
 class MainMap extends React.Component {
+  state = {
+    markers: [],
+  }
+
+  addMarker = e => {
+    const { markers } = this.state
+    markers.push(e.latlng)
+    this.setState({ markers })
+  }
+
   render() {
     if (typeof window !== 'undefined') {
       return (
-        <Map id="mapid" crs={L.CRS.Simple} minZoom={-2} bounds={bounds}>
-          <ImageOverlay
-            minZoom={-2}
-            bounds={bounds}
-            maxBounds={bounds}
-            url={RDRMapFull}
-          />
+        <Map
+          id="mapid"
+          crs={L.CRS.Simple}
+          minZoom={-2}
+          onClick={this.addMarker}
+          bounds={bounds}
+        >
+          <ImageOverlay bounds={bounds} maxBounds={bounds} url={RDRMapFull} />
+          {this.state.markers.map((position, idx) => (
+            <Marker key={`marker-${idx}`} position={position}>
+              <Popup>
+                <span>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </span>
+              </Popup>
+            </Marker>
+          ))}
         </Map>
       )
     }
-    return (
-      <div>
-        <p>Wrong</p>
-      </div>
-    )
+    return null
   }
 }
 
